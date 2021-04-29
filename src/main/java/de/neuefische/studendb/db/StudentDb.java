afd578021bbc16dc87d3a6ed805b5fad6ce3709d
christianp <porsc.chr@gmail.com>
2021-04-29 16:12:44 +0200

    Add hashmap

--- src/main/java/de/neuefische/studendb/db/StudentDb.java
+++ src/main/java/de/neuefische/studendb/db/StudentDb.java
@@ -3,32 +3,23 @@ package de.neuefische.studendb.db;
 import de.neuefische.studendb.model.Student;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StudentDb {
 
-    /*private Student [] students;*/
+    private Map<String, Student> mapOfStudents = new HashMap<>();
 
-    private ArrayList<Student> students = new ArrayList<Student>();
-
-    public StudentDb(ArrayList<Student> students) {
-        this.students = students;
+    public StudentDb(Map<String, Student> mapOfStudents) {
+        this.mapOfStudents = mapOfStudents;
     }
 
-
-    /* public StudentDb(Student[] students) {
-        this.students = students;
-    }*/
-
-    /*public Student[] list() {
-        return students;
-    }*/
-
-    public ArrayList<Student> list() {
-        return students;
+    public Map<String, Student> list() {
+        return mapOfStudents;
     }
 
     public Student findById(String id){
-        for (Student currentStudent : students) {
+        for (Student currentStudent : mapOfStudents) {
             if (id.equals(currentStudent.getId())){
                 return currentStudent;
             }
@@ -40,70 +31,32 @@ public class StudentDb {
     @Override
     public String toString() {
         String result = "";
-        for (int i = 0 ; i < students.size(); i++) {
-            result += students.get(i) + "\n";
+        for (int i = 0 ; i < mapOfStudents.size(); i++) {
+            result += mapOfStudents.get(i) + "\n";
         }
         return result;
     }
 
 
-
-    /*    @Override
-    public String toString(){
-        String result = "";
-        for (int i = 0; i < students.length; i++) {
-            result += students[i] + "\n";
-        }
-        return result;
-    }*/
 
     public Student randomStudent() {
-        int index = (int) Math.floor(Math.random() * students.size());
-        return students.get(index);
+        int index = (int) Math.floor(Math.random() * mapOfStudents.size());
+        return mapOfStudents.get(index);
     }
 
-    /*public void add(Student student) {
-        Student[] updatedStudents = new Student[students.length+1];
-
-        for (int i = 0; i < students.length; i++) {
-            updatedStudents[i] = students[i];
-        }
-
-        updatedStudents[updatedStudents.length-1] = student;
-
-        students = updatedStudents;
-    }*/
 
     public void add(Student student){
-        this.students.add(student);
+        this.mapOfStudents.put(student.getId(), student);
     }
 
-    /*public void remove(Student student) {
-        int foundIndex = findIndex(student);
-        if(foundIndex < 0){
-            return;
-        }
-        Student[] updatedStudents = new Student[students.length-1];
-
-        for (int i = 0; i < students.length; i++) {
-            if(i < foundIndex){
-                updatedStudents[i] = students[i];
-            }
-            if(i > foundIndex){
-                updatedStudents[i-1] = students[i];
-            }
-        }
-
-        students = updatedStudents;
-    }*/
 
     public void remove(Student student){
-        this.students.remove(student);
+        this.mapOfStudents.remove(student);
     }
 
     private int findIndex(Student student) {
-        for (int i = 0; i < students.size(); i++) {
-            if(students.get(i).equals(student)){
+        for (int i = 0; i < mapOfStudents.size(); i++) {
+            if(mapOfStudents.get(i).equals(student)){
                 return i;
             }
         }
